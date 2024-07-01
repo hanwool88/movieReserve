631f85eaebd2a522b01e80284b9d536f7bba60a6
hanwool88 han <hanwool950913@gmail.com>
2024-07-01 04:58:32 +0000

    commit

---
+++ movie/src/main/java/finalproject/domain/ReserveCanceled.java
@@ -0,0 +1,21 @@
+package finalproject.domain;
+
+import finalproject.domain.*;
+import finalproject.infra.AbstractEvent;
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class ReserveCanceled extends AbstractEvent {
+
+    private Long id;
+    private String userId;
+    private String reserveStatus;
+    private Integer amount;
+    private Date reserveDate;
+    private Long movieId;
+}
+//>>> DDD / Domain Event
